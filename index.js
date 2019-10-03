a924cc02a7dc61b22608140d3b354f941bfe42e8
MichaelBaynon <baynonmichael@gmail.com>
2019-10-03 02:04:06 -0400

    validate user id working

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// code away!
+const server = require('./server')
 
-const express = require("express");
-const path = require('path')
-
-const port = 8888;
-
-const server = express();
-server.use(express.json());
-
-function logger(req, res, next) {
-  console.log(
-    `[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get(
-      "Origin"
-    )} `
-  );
-  next();
-}
-
-function validateUserId(req, res, next) {
-    const userid = req.params.id
-
-    if(!userid || userid !== req.params.id) {
-      return res.status(401).json({message: 'user id is not valid'})
-    }
-    
-    next()
-}
-
-server.use(logger);
-
-server.listen(port, () => {
-  console.log(`server listening on port ${port}`);
-});
+server.listen(8888, () => console.log('server listening on port 8888'))
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,6 @@
-const express = 'express';
+const express = require('express');
 
+const Users = require('./userDb')
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -11,11 +12,19 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    Users.get()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: 'error getting users'})
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId,  (req, res) => {
+    console.log('received user')
+res.status(200).json(req.user)
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -26,15 +35,42 @@ router.delete('/:id', (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
+    const id = req.params.id
+    const {name} = req.body
 
+    Users.update(id, {name})
+    .then(updated => {
+        if (updated) {
+            Users.getById(id)
+            .then(user => res.status(200).json(user))
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({error: 'error getting user'})
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: 'error updating user'})
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const {id} = req.params
 
-};
+   Users.getById(id)
+   .then(user => {
+       if (user) {
+           req.user = user
+           next()
+       } else {
+           res.status(404).json({error: 'user with id doesnt exist'})
+       }
+   })
+}
 
 function validateUser(req, res, next) {
 
